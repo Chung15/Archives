352c87c9b6ced0c7043db4e03af3b4934e1c672c
Chung <Chung>
2016-11-19 03:40:50 +0200

    first commit with fisrt master page and bootstrap files added

---
+++ resources/views/master.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>App Name - @yield('title')</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <link href="css/style.css" rel="stylesheet">
+        <link href="css/bootstrap.css" rel="stylesheet">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Styles -->
+        
+    </head>
+    <body>
+    <div class="header">
+      @yield('header')
+    </div>
+    <div class="content">
+        @yield('content')
+    </div>
+    <div class="aside_content">
+        @yield('aside_content')
+    </div>
+   
+    <div class="footer">
+     @yield('footer')
+    </div>
+   
+
+   
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/scripts.js"></script>
+    </body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+@extends('master')
+
+    @section('title')
+     Archives
+    @endsection
+
+    @section('content')
+        <h1> Welcome to laravel </h1>
+    @endsection
